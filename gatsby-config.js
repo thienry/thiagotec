cdd5dd0ba1bbf2b1ed53448c42043b4b77758247
Thiago Moura <thmoura14@gmail.com>
2020-05-31 19:50:09 -0300

    fix: changed background & theme color of the app

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,8 +23,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#FF8585`,
-        theme_color: `#FF8585`,
+        background_color: `#162447`,
+        theme_color: `#162447`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
