21b46e6c3006cb2da20616674607d52b89337011
Thiago Moura <33002497+thienry@users.noreply.github.com>
2021-05-12 19:22:32 -0300

    Update UseEffect in layout.js

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -37,7 +37,7 @@ const Layout = ({ children, styleNav, styleContainer, isActive }) => {
       scrollTop.classList.toggle('scroll-to-top', window.scrollY > 700)
       scrollTop.classList.toggle('scroll-fadeout', window.scrollY < 800)
     })
-  })
+  }, [])
 
   function toggleSidebar() {
     setIsOpen(!isOpen)
